908dd3a87d4416831621699d027807a99364a9e6
Kobe Ponet <kobe.ponet@student.kdg.be>
2024-10-21 23:13:28 +0200

    added logger to completePurchaseOrder

--- src/main/java/kdg/be/water/service/PurchaseOrderService.java
+++ src/main/java/kdg/be/water/service/PurchaseOrderService.java
@@ -1,7 +1,10 @@
 package kdg.be.water.service;
 
 import kdg.be.water.controller.dto.PurchaseOrderCompleteDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,19 +14,27 @@ import java.util.UUID;
 @Service
 public class PurchaseOrderService {
 
+    private static final Logger logger = LoggerFactory.getLogger(PurchaseOrderService.class);
+
     private final RestTemplate restTemplate;
 
+    @Autowired
     public PurchaseOrderService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
-    //TODO: when to call this method?
     public void completePurchaseOrder(UUID sellerId, List<String> poNumbers) {
         String url = "http://localhost:8082/api/purchase-orders/complete";
-        PurchaseOrderCompleteDTO request = new PurchaseOrderCompleteDTO();
-        request.setSellerId(sellerId);
-        request.setPoNumbers(poNumbers);
+        PurchaseOrderCompleteDTO request = new PurchaseOrderCompleteDTO(sellerId, poNumbers);
 
-        restTemplate.postForObject(url, request, Void.class);
+        logger.info("Sending request to complete purchase order with sellerId: {} and poNumbers: {}", sellerId, poNumbers);
+
+        ResponseEntity<Void> response = restTemplate.postForEntity(url, request, Void.class);
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            logger.info("Purchase order completed successfully.");
+        } else {
+            logger.error("Failed to complete purchase order. Status code: {}", response.getStatusCode());
+        }
     }
 }
